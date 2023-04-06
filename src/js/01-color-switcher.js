0d8bece10b7d1ed653c4fe8b7d3dc3e30b5f6def
RenTensei <scs.bon7@gmail.com>
2023-04-06 23:02:01 +0300

    9.1 solution

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let intervalId = null;
+stopBtn.disabled = true;
+
+function startHandler() {
+  if (intervalId) {
+    return;
+  }
+  switchButtons();
+
+  body.style.backgroundColor = getRandomHexColor();
+
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopHandler() {
+  switchButtons();
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function switchButtons() {
+  stopBtn.disabled = !stopBtn.disabled;
+  startBtn.disabled = !startBtn.disabled;
+}
+
+startBtn.addEventListener('click', startHandler);
+stopBtn.addEventListener('click', stopHandler);
