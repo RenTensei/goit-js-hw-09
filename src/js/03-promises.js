0b1306ed094b2158c50ed85f1fab241f0ffecaf6
RenTensei <scs.bon7@gmail.com>
2023-04-08 18:36:15 +0300

    9.3 solution

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  // get data from form
+  const FD = new FormData(formRef);
+  const delay = parseInt(FD.get('delay'));
+  const step = parseInt(FD.get('step'));
+  const amount = parseInt(FD.get('amount'));
+
+  for (let i = 0; i < amount; i++) {
+    const currentPosition = i + 1;
+    const totalDelay = i * step + delay;
+
+    createPromise(currentPosition, totalDelay)
+      .then(({ position, delay }) => {
+        Notify.success('it worked!');
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure('something went wrong :(');
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+formRef.addEventListener('submit', handleSubmit);
